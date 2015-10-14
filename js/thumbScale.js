1f6f9db437805f3d18cc0e75c72b94d0ea3cc1f0
Martijn Reeuwijk <martijn.reeuwijk@kickorstick.com>
2015-10-14 12:12:29 +0200

    Eerste
    upload

---
+++ js/thumbScale.js
@@ -0,0 +1,6 @@
+$(window).load(function() {
+    $('.thumb').find('img').each(function() {
+        var imgClass = (this.width / this.height > 1) ? 'wide' : 'tall';
+        $(this).addClass(imgClass);
+    })
+})
