d84ad405083a3276e9539f07fb300b60d838fb45
Martijn Reeuwijk <martijn.reeuwijk@kickorstick.com>
2015-10-14 12:17:14 +0200

    Delete
    Delete van de git

--- js copy/thumbScale.js
+++
@@ -1,6 +0,0 @@
-$(window).load(function() {
-    $('.thumb').find('img').each(function() {
-        var imgClass = (this.width / this.height > 1) ? 'wide' : 'tall';
-        $(this).addClass(imgClass);
-    })
-})
